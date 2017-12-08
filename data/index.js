175c07779dd9854604ca51ba6126c5c19a51bb90
sirgalleto <sebastian.osorio@wizeline.com>
2017-12-08 14:39:08 -0600

    Create a index to sport all data

---
+++ data/index.js
@@ -0,0 +1,11 @@
+import communities from './communities';
+import rooms from './rooms';
+import schedule from './schedule';
+import talkTypes from './talkTypes';
+
+export {
+  communities,
+  rooms,
+  schedule,
+  talkTypes
+};
